c715e488c5d15c247e3cd72ef8dcad8b3bb78418
anafmartins8 <asfmartins@uporto.pt>
2023-05-03 09:30:13 +0100

    feat: add buttons to to image control

--- src/layout/zoom-area/ZoomArea.js
+++ src/layout/zoom-area/ZoomArea.js
@@ -1,9 +1,17 @@
 import { RiZoomInLine } from "react-icons/ri";
 import { RiZoomOutLine } from "react-icons/ri";
+import { MdOutlineLoop } from "react-icons/md"; //MdCached
+import { FaVectorSquare } from "react-icons/fa";
+import { TbArrowRightSquare } from "react-icons/tb"; //BsBoundingBoxCircles BsBoundingBox
 
 function ZoomArea() {
   return (
     <div className="zoom-container">
+      <div>
+        <button type="button" className="button-info">
+          <MdOutlineLoop />
+        </button>
+      </div>
       <div>
         <button type="button" className="button-info">
           <RiZoomInLine />
@@ -12,6 +20,16 @@ function ZoomArea() {
       <div>
         <button type="button" className="button-info">
           <RiZoomOutLine />
+        </button>
+      </div>
+      <div>
+        <button type="button" className="button-info">
+          <TbArrowRightSquare />
+        </button>
+      </div>
+      <div>
+        <button type="button" className="button-info square-icon">
+          <FaVectorSquare />
         </button>
       </div>
     </div>
